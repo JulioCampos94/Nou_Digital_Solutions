d3af0ce43afe09269053bf9d299912a33bab4b75
julio campos <juliocampos@julios-MacBook-Air.local>
2025-07-09 20:22:23 -0600

    Formulario

--- nds_logic.js
+++ nds_logic.js
@@ -1,2 +1,11 @@
 // Ejemplo de comportamiento: mensaje en consola
 console.log("Nou Digital Solutions – página cargada correctamente.");
+
+
+const form = document.getElementById('diagnosticoForm');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  alert('¡Gracias por enviar tu diagnóstico! Nos pondremos en contacto contigo pronto.');
+  form.reset();
+});
