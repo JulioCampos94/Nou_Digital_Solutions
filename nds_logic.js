fa8070c8bdf89e57a253aaf90db861c7fad083a4
julio campos <juliocampos@julios-MacBook-Air.local>
2025-07-17 19:55:31 -0600

    responsivo

--- nds_logic.js
+++ nds_logic.js
@@ -1,11 +1,15 @@
-// Ejemplo de comportamiento: mensaje en consola
+// Mensaje al cargar la página
 console.log("Nou Digital Solutions – página cargada correctamente.");
 
-
+// Enviar formulario
 const form = document.getElementById('diagnosticoForm');
-
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   alert('¡Gracias por enviar tu diagnóstico! Nos pondremos en contacto contigo pronto.');
   form.reset();
+});
+
+// Toggle menú hamburguesa
+document.getElementById('navbar-toggle').addEventListener('click', function () {
+  document.getElementById('navbar-list').classList.toggle('show');
 });
